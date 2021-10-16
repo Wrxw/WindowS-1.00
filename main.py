138affe917e717c6070266be06fcadc5e0224c23
Wrxw <wiktor.wolos@psp9.radom.pl>
2021-10-16 12:31:42 +0200

    short creation 1

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-CreationO = open(file="SettingsNotEditThis\Creation.txt",mode="r")
+CreationO = open(file="Creation.txt",mode="r")
 Creation = CreationO.readline(1)
 #^^^Importing files^^^
 #\/Short creation\/
+if Creation == 0:
+    print("Welcome plese add informations to use this system")
+    
+
 
